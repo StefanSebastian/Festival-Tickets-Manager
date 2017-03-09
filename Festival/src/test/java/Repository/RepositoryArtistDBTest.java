bc7c5348e49ffc0ebe23c5c4b71f3263e790c7d6
StefanSebastian <stefansebii@gmail.com>
2017-03-09 21:46:34 +0200

    Added repository testing

---
+++ Festival/src/test/java/Repository/RepositoryArtistDBTest.java
@@ -0,0 +1,81 @@
+package Repository;
+
+import Domain.Artist;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sebi on 09-Mar-17.
+ */
+public class RepositoryArtistDBTest {
+    RepositoryArtistDB repo;
+
+    @Before
+    public void setUp() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("jdbc.driver", "com.mysql.jdbc.Driver");
+        properties.setProperty("jdbc.url", "jdbc:mysql://localhost/test_festival");
+        properties.setProperty("jdbc.user", "root");
+        properties.setProperty("jdbc.pass", "pass");
+        repo = new RepositoryArtistDB(properties);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        for (Artist a : repo.getAll()){
+            repo.delete(a.getIdArtist());
+        }
+        repo = null;
+    }
+
+    @Test
+    public void save() throws Exception {
+        repo.save(new Artist(1, "name"));
+        Artist a = repo.getById(1);
+        assertTrue(a.getName().equals("name"));
+        assertTrue(a.getIdArtist() == 1);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        repo.save(new Artist(1, "name"));
+        Integer size = repo.getAll().size();
+        assertTrue(size == 1);
+        repo.delete(1);
+        size = repo.getAll().size();
+        assertTrue(size == 0);
+    }
+
+    @Test
+    public void update() throws Exception {
+        repo.save(new Artist(1, "name"));
+        repo.update(1, new Artist(2, "altNume"));
+        Artist a = repo.getById(2);
+        assertTrue(a.getName().equals("altNume"));
+    }
+
+    @Test
+    public void getById() throws Exception {
+        repo.save(new Artist(1, "name"));
+        Artist a = repo.getById(1);
+        assertTrue(a.getIdArtist() == 1);
+        a = repo.getById(2);
+        assertTrue(a == null);
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        repo.save(new Artist(1, "name"));
+        repo.save(new Artist(2, "name"));
+
+        assertTrue(repo.getAll().size() == 2);
+    }
+
+}
---
+++ Festival/src/test/java/Repository/RepositoryShowDBTest.java
@@ -0,0 +1,95 @@
+package Repository;
+
+import Domain.Artist;
+import Domain.Show;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sebi on 09-Mar-17.
+ */
+public class RepositoryShowDBTest {
+    RepositoryArtistDB repoArtist;
+    RepositoryShowDB repoShow;
+
+    @Before
+    public void setUp() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("jdbc.driver", "com.mysql.jdbc.Driver");
+        properties.setProperty("jdbc.url", "jdbc:mysql://localhost/test_festival");
+        properties.setProperty("jdbc.user", "root");
+        properties.setProperty("jdbc.pass", "pass");
+        repoArtist = new RepositoryArtistDB(properties);
+        repoShow = new RepositoryShowDB(properties);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        for (Show s : repoShow.getAll()){
+            repoShow.delete(s.getIdShow());
+        }
+        repoShow = null;
+
+        for (Artist a : repoArtist.getAll()){
+            repoArtist.delete(a.getIdArtist());
+        }
+        repoArtist = null;
+    }
+
+    @Test
+    public void save() throws Exception {
+        repoArtist.save(new Artist(1, "name"));
+        repoShow.save(new Show(1, "loc", "2016-03-03 20:00", 12, 0, 1));
+
+        assertTrue(repoShow.getById(1).getIdArtist() == 1);
+        assertTrue(repoShow.getById(1).getLocation().equals("loc"));
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+        repoArtist.save(new Artist(1, "name"));
+        repoShow.save(new Show(1, "loc", "2016-03-03 20:00", 12, 0, 1));
+
+        assertTrue(repoShow.getAll().size() == 1);
+        repoShow.delete(1);
+        assertTrue(repoShow.getAll().size() == 0);
+    }
+
+    @Test
+    public void update() throws Exception {
+        repoArtist.save(new Artist(1, "name"));
+        repoShow.save(new Show(1, "loc", "2016-03-03 20:00", 12, 0, 1));
+
+        repoShow.update(1, new Show(1, "locul", "2016-03-03 21:00", 22, 0, 1));
+        assertTrue(repoShow.getById(1).getLocation().equals("locul"));
+
+    }
+
+    @Test
+    public void getById() throws Exception {
+        repoArtist.save(new Artist(1, "name"));
+        repoShow.save(new Show(1, "loc", "2016-03-03 20:00", 12, 0, 1));
+
+        assertTrue(repoShow.getById(1).getLocation().equals("loc"));
+        assertTrue(repoShow.getById(2) == null);
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        repoArtist.save(new Artist(1, "name"));
+
+        assertTrue(repoShow.getAll().size() == 0);
+        repoShow.save(new Show(1, "loc", "2016-03-03 20:00", 12, 0, 1));
+        repoShow.save(new Show(2, "loc", "2016-03-03 21:00", 12, 0, 1));
+        assertTrue(repoShow.getAll().size() == 2);
+
+    }
+
+}
